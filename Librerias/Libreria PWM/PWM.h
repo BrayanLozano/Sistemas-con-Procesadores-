add933866a9d4b05ebe807cabc71753e2dd726f7
Brayan Lozano <brayan_lozano@outlook.es>
2017-06-06 12:52:25 -0500

    Add files via upload

---
+++ Librerias/Libreria PWM/PWM.h
@@ -0,0 +1,10 @@
+#ifndef Funciones_H
+#define Funciones_H
+
+void initPWM();
+void initDutyCycle(char Buf, int util, int per);
+void initSetper(int numHerz,int escaler);
+void initEnableDisable(char chanel , int OnOff);
+
+
+#endif 
